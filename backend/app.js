a9d7d3321e39cae63da5be9b448aaf544498efc8
edwin <emash90@gmail.com>
2022-05-12 21:42:57 +0300

    fixing cors issues

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,7 @@ const urlencoded = require('body-parser/lib/types/urlencoded')
 
 const io = new Server(server, {
     cors: {
+        "origin": "http://localhost:3000",
         methods: ['GET', 'POST', 'PATCH', 'DELETE']
     }
 })
