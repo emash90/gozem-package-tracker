345919e567d6f3ac4a2e23e7509f20a31a9b8b28
edwin <emash90@gmail.com>
2022-05-12 07:16:07 +0300

    working on deployment

--- backend/app.js
+++ backend/app.js
@@ -25,13 +25,13 @@ const port = process.env.PORT || 9900
 app.use(morgan('dev'))
 app.use(cors(corsOptions))
 app.use(express.json());
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views') )
+// app.set('view engine', 'ejs')
+// app.set('views', path.join(__dirname, 'views') )
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/package', require('./routes/packageRoutes'))
 app.use('/api/delivery', require('./routes/deliveryRoutes'))
-app.use(express.static(path.join(__dirname, '../frontend/build')))
+if(process.env.NODE_ENV === 'production'){app.use(express.static(path.join(__dirname, '../frontend/build')))}
 
 app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
 app.use(errorHandler)
