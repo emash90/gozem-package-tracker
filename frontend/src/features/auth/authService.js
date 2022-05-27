b01951fb55d023b92b695693a69b10c484d076bc
edwin <emash90@gmail.com>
2022-05-27 10:48:16 +0300

    fixed heroku deploying issues

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-export const baseURL = process.env.REACT_APP_BASE_URL;
+//export const baseURL = process.env.REACT_APP_BASE_URL;
 
 const API_URL = 'https://packagedeliveryapp.herokuapp.com/'
 
